9679e5056ff3c182acbe8f56672c720bf6716f57
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-24 13:47:08 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ let form = document.querySelector('form')
+ form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  handleToDo(e.target.new_task_description.value)
+  form.reset()
+ })
 });
+function handleToDo(todo){
+  //console.log(todo)
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
